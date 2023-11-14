185c844be45d860ce4e990764d966e8c17863648
EsraYalcin <yalcinaceresra@gmail.com>
2023-11-14 14:09:50 +0100

    add axios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,31 +4,26 @@ import axios from "axios";
 const Home = () => {
  
     const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-      axios("https://randomuser.me/api/?results=10")
-        .then((response) =>
-          response.data.results.map((user) => ({
-            name: `${user.name.first} ${user.name.last}`,
-            username: `${user.login.username}`,
-            email: `${user.email}`,
-            image: `${user.picture.large}`,
-          }))
-        )
-        .then((data) => {
-          setUsers(data);
-        });
-    }, []);
+const BASE_URL="https://randomuser.me/api/?results=10"
+const getRandomUser=async()=>{
+  const veri=await axios.get(BASE_URL)
+  console.log(veri.data.results);
+  setUsers(veri.data.results)
+}
+useEffect(() => {
+  getRandomUser();
+}, []);
+   
   
     return (
       <div className="users text-center">
       <h1 className='text-center text-warning'>RANDOM USER</h1>
-      <button className='btn btn-success' onClick={()=>{ window.location.reload()}}>Getir</button>
+      <button className='btn btn-success' onClick={()=>{ getRandomUser()}}>Getir</button>
         {users.map((user) => (
-          <div key={user.username} className="users__user container w-50 mb-4">
-            <img src={user.image} className="users__avatar" alt="#" />
+          <div key={user.login.username} className="users__user container w-50 mb-4">
+            <img src={user.picture.large} className="users__avatar" alt="#" />
             <div className="users__meta">
-              <h1>{user.name}</h1>
+              <h1>{user.name.first}</h1>
               <p>{user.email}</p>
             </div>
           </div>
